a53a8d3b872b40e26772d45ea1b509c89a2987fc
Fred <fred_p1@hotmail.co.uk>
2019-07-11 20:24:32 +0100

    Website Working, first iteration

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Fred Palmer</h1>
+                <p>Black space to put important summary of what this website is.</p>
             </div>
         </div>
         <nav>
