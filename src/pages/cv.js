939bb47fb3e8455bf36e9e9c3b58eea3efa4a0e4
Fred <fred_p1@hotmail.co.uk>
2019-11-12 19:10:07 +0000

    Update cv.js

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Pdf from '../documents/FredPalmerCV.pdf';
-import '../assets/scss/layout/_cv.scss';
+import Pdf from '../documents/FredPalmerCV.pdf'
+import '../assets/scss/layout/_cv.scss'
 
-const cvPage = () =>(
-
-        <iframe src={Pdf} style="width: 100%;height: 100%;border: none;"></iframe>
+const cvPage = () => (
+  <body>
+<iframe src={Pdf}>
+</iframe>
+  </body>
 )
-export default cvPage;
+export default cvPage
