aa032d3f4b4aa5300b2b20fbcf9477deb1efdae0
Fred <fred_p1@hotmail.co.uk>
2019-11-12 19:29:43 +0000

    Update cv.js

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -5,7 +5,7 @@ import '../assets/scss/layout/_cv.scss'
 const cvPage = () => (
   <body>
     <div>
-      <iframe src={Pdf}></iframe>
+      <iframe width = "100%" height = "100%" src={Pdf}></iframe>
     </div>
   </body>
 )
