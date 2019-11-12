9658e5aca6dc656c56c59ebff76218f763af2235
Fred <fred_p1@hotmail.co.uk>
2019-11-12 19:22:56 +0000

    Update cv.js

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -3,10 +3,10 @@ import Pdf from '../documents/FredPalmerCV.pdf'
 import '../assets/scss/layout/_cv.scss'
 
 const cvPage = () => (
-    <style>body {width: 100%; height: 100%; margin: 0; padding: 0;}</style>
   <body>
-<iframe src={Pdf}>
-</iframe>
+    <div>
+      <iframe src={Pdf}></iframe>
+    </div>
   </body>
 )
 export default cvPage
