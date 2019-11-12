6ff4b199345aa19082cc6ec6d92f1ab4b5cbe354
Fred <fred_p1@hotmail.co.uk>
2019-11-12 19:04:42 +0000

    Update cv.js

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -3,8 +3,7 @@ import Pdf from '../documents/FredPalmerCV.pdf';
 import '../assets/scss/layout/_cv.scss';
 
 const cvPage = () =>(
-    <body>
+
         <iframe src={Pdf} style="width: 100%;height: 100%;border: none;"></iframe>
-    </body>
 )
 export default cvPage;
