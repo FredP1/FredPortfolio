ebe232fee79900ca3682290fc8e0a1d71e46f505
Fred <fred_p1@hotmail.co.uk>
2019-11-13 16:17:07 +0000

    Update cv.js

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Document } from 'react-pdf'
+import { Document, Page } from 'react-pdf'
 import Pdf from '../documents/FredPalmerCV.pdf'
 import '../assets/scss/layout/_cv.scss'
 
