c3b7ed795af63ad14c6b3b9741527490802bb0cf
Fred <fred_p1@hotmail.co.uk>
2019-11-13 17:17:12 +0000

    Update cv.js

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Document, Page } from 'react-pdf'
 import Pdf from '../documents/FredPalmerCV.pdf'
 import '../assets/scss/layout/_cv.scss'
 
-const cvPage = () => (
-  <html height="100%" min-height="100%">
-    <body width="100%" height="100%" min-height="100%">
-      <div width="100%" height="100%" min-height="100%">
-        <Document file="./1.pdf" onLoadSuccess={this.onDocumentLoad}>
-          <Page pageNumber={pageNumber} />
-        </Document>
-      </div>
-    </body>
-  </html>
-)
+class cvPage extends Component {
+    state = {
+      numPages: null,
+      pageNumber: 1,
+    }
+   
+    onDocumentLoadSuccess = ({ numPages }) => {
+      this.setState({ numPages });
+    }
+   
+    render() {
+      const { pageNumber, numPages } = this.state;
+   
+      return (
+        <div div width="100%" height="100%" min-height="100%">
+          <Document
+            file={Pdf}
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} />
+          </Document>
+        </div>
+      );
+    }
+  }
 export default cvPage
