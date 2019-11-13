ec38003b2f1f8a6f27813c5c2f3ee29439900799
Fred <fred_p1@hotmail.co.uk>
2019-11-13 16:13:29 +0000

    Update cv.js

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -7,7 +7,7 @@ const cvPage = () => (
   <html height="100%" min-height = "100%">
     <body width="100%" height="100%" min-height = "100%">
       <div width="100%" height="100%" min-height = "100%">
-      <Document file={Pdf} />
+      <Document file={Pdf} onLoadSuccess={this.onDocumentLoadSuccess}/>
       </div>
     </body>
   </html>
