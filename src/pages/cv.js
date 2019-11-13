48a61cdbbbb6a6b3cdf82fe917beb79dc880d9dc
Fred <fred_p1@hotmail.co.uk>
2019-11-13 16:15:32 +0000

    Update cv.js

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -4,10 +4,12 @@ import Pdf from '../documents/FredPalmerCV.pdf'
 import '../assets/scss/layout/_cv.scss'
 
 const cvPage = () => (
-  <html height="100%" min-height = "100%">
-    <body width="100%" height="100%" min-height = "100%">
-      <div width="100%" height="100%" min-height = "100%">
-      <Document file={Pdf} onLoadSuccess={this.onDocumentLoadSuccess}/>
+  <html height="100%" min-height="100%">
+    <body width="100%" height="100%" min-height="100%">
+      <div width="100%" height="100%" min-height="100%">
+        <Document file="./1.pdf" onLoadSuccess={this.onDocumentLoad}>
+          <Page pageNumber={pageNumber} />
+        </Document>
       </div>
     </body>
   </html>
