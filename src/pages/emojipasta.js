5a30b3ad961b64cd38024ec09781976c3b2a64c5
Fred <fred_p1@hotmail.co.uk>
2019-11-23 10:19:27 +0000

    very much working now
    just need to add more emoji's

--- src/pages/emojipasta.js
+++ src/pages/emojipasta.js
@@ -3,30 +3,32 @@ import Header from '../components/Header'
 import '../assets/scss/main.scss'
 
 class emojipasta extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+  constructor(props) {
+    super(props)
+    this.state = { value: '' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.myRef = React.createRef()
+    this.state = {conversion: ''}
+  }
   componentDidMount() {
     {
       //this.emojiPasta()
     }
   }
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value })
   }
-  handleSubmit(event){
-      this.emojiPasta(this.state.value)
-      event.preventDefault();
+  handleSubmit(event) {
+    var returnValue = this.emojiPasta(this.state.value)
+    this.setState({ conversion: returnValue})
+    event.preventDefault()
   }
   emojiPasta(text) {
     text = text + ''
     text = text.toLowerCase()
     var textArray = text.toString().split(' ')
-    var emojiDict = { happy: ['😃'] }
+    var emojiDict = { happy: ['😃'], penis: ['🍆'] }
     var finalString = ''
 
     for (var i = 0; i < textArray.length; i++) {
@@ -35,15 +37,24 @@ class emojipasta extends Component {
         finalString += emojiDict[textArray[i]]
       }
     }
+    return finalString;
     window.alert(finalString)
   }
   render() {
     return (
+      <div>
         <form onSubmit={this.handleSubmit}>
-            Enter Text:
-            <input type="text" value={this.state.value} onChange={this.handleChange}></input>
-            <input type="submit" name = "submit"></input>
+          Enter Text:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            ref = {this.myRef}
+          ></input>
+          <input type="submit" name="submit"></input>
         </form>
+        <p ref={this.myRef}>Conversion: {this.state.conversion}</p>
+      </div>
     )
   }
 }
