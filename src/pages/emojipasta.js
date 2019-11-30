ec2f3bb0919e367ebfae944339f115e8e3ee7980
Fred <fred_p1@hotmail.co.uk>
2019-11-30 10:37:48 +0000

    finished, still needs some formatting work

--- src/pages/emojipasta.js
+++ src/pages/emojipasta.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
 import '../assets/scss/main.scss'
+import emojijsonimport from '../documents/emoji.json'
 
 function emojipasta() {
   const [inputText, setInputText] = useState('')
   const [outputText, setOutputText] = useState('')
-  function nestedFunction(){
-    var inputShit = inputText;
+  const emojijson = emojijsonimport
+  function nestedFunction(text){
+    var inputShit = text;
     inputShit = inputShit.toLowerCase()
     var textArray = inputShit.toString().split(' ')
-    var emojiDict = { happy: ['😃 '], penis: ['🍆 '], funny: ['😂 '], wet: ['💦 '] }
     var finalString = ''
 
     for (var i = 0; i < textArray.length; i++) {
       finalString += '' + textArray[i] + ' '
-      if (emojiDict.hasOwnProperty(textArray[i])) {
-        finalString += emojiDict[textArray[i]]
+      if (emojijson.hasOwnProperty(textArray[i])) {
+        finalString += emojijson[textArray[i]]
       }
     }
     //setOutputText(finalString)
@@ -26,7 +27,7 @@ function emojipasta() {
         <input
           type="text"
           value={inputText}
-          onChange={e => {setInputText(e.target.value); setOutputText(nestedFunction())}}
+          onChange={e => {setInputText(e.target.value); setOutputText(nestedFunction(e.target.value))}}
         ></input>
       <h2>Conversion: </h2>
       <input type ="text" value={outputText} readOnly></input>
