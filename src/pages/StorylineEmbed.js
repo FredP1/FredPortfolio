dcc4eee2ff828d335f95b903dc3d85f80d4c7c8f
Fred <fred_p1@hotmail.co.uk>
2024-10-30 19:00:00 +0000

    test

--- src/pages/StorylineEmbed.js
+++ src/pages/StorylineEmbed.js
@@ -5,7 +5,7 @@ const StorylineEmbed = () => {
   return (
     <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
       <iframe
-        src={`${process.env.PUBLIC_URL}/storyline/story.html`}
+        src={`${process.env.PUBLIC_URL}/prototype/story.html`}
         title="Articulate Storyline"
         style={{ width: '100%', height: '100%', border: 'none' }}
       />
